67b2db1886253e8b4452ac2670019ce68d3cd9dc
Ingkarat Tin <104818213+ingkaratt@users.noreply.github.com>
2024-10-01 14:56:23 +0700

    update api url#2

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,7 +3,7 @@ export const API_URL =
 export const API_SHEET = 'https://carrent-sheet.herokuapp.com/api/updatesheet'
 // export const API_STOCK =
 // 'https://stock-amazon-98bae8019fb5.herokuapp.com/api/v1'
-export const API_STOCK = 'https://car-rent-api-eight.vercel.app/'
+export const API_STOCK = 'https://car-rent-api-eight.vercel.app'
 export const API_AUTH_TEST = 'https://car-rent-api-eight.vercel.app/auth'
 // export const API_AUTH_TEST =
 //   'https://stock-amazon-98bae8019fb5.herokuapp.com/api/v1/auth'
