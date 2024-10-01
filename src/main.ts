34cece3c4bcb1c419696f385d929a1ac3e7511ac
Ingkarat Tin <104818213+ingkaratt@users.noreply.github.com>
2024-10-01 12:17:25 +0700

    add env

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,22 @@ import '@/styles/tailwind.scss'
 import VCalendar from 'v-calendar'
 import 'v-calendar/style.css'
 import { initializeApp } from 'firebase/app'
-import { firebaseConfig } from './assets/config'
+// import { firebaseConfig } from './assets/config'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(VCalendar, {})
-export const firebase = initializeApp(firebaseConfig)
+export const firebase = initializeApp({
+  apiKey: import.meta.env.API_KEY,
+  authDomain: import.meta.env.AUTH_DOMAIN,
+  databaseURL: import.meta.env.DATABASE_URL,
+  projectId: import.meta.env.PROJECT_ID,
+  storageBucket: import.meta.env.STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.MESSAGING_SENDER_ID,
+  appId: import.meta.env.APP_ID,
+  measurementId: import.meta.env.MEASUREMENT_ID,
+})
 
 app.mount('#app')
